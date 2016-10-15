3bca58419ec1038d5d7b64a1ed76d967d6d24c4c
Johannes Axner <johannes.axner@gmail.com>
2016-10-15 15:35:22 +0200

    First draft.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+// Convert timestamps
+moment.locale('sv');
+var timestamps = document.getElementsByClassName('timestamp');
+var i = 0;
+for (var a in timestamps) {
+    var time = moment().subtract(i, 'minutes');
+    timestamps[a].text = moment(time).fromNow();
+    i += Math.floor(Math.random()*10)+1;
+};
